6cbc350ee0bd53f82d1c77189bb7af4a7bd1a9b5
furkaner <furkan08_@hotmail.com>
2022-04-29 02:22:03 +0300

    Update routers.jsx

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import AccountBidCard from '../components/AccountBidCard/AccountBidCard'
 import BidModal from '../components/BidModal/BidModal'
 import Card from '../components/Card/Card'
 import Navbar from '../components/Navbar/Navbar'
+import AccountPage from '../pages/AccountPage/AccountPage'
 
 import HomePage from '../pages/HomePage/HomePage'
 import ProductDetailPage from '../pages/ProductDetail/ProductDetailPage'
+import ProductUpload from '../pages/ProductUploadPage/ProductUpload'
 import SignIn from '../pages/SignIn/SignIn'
 import SignUp from '../pages/SignUp/SignUp'
 
 function Routers() {
     return (
      <Routes>
-        <Route path="/" element={<ProductDetailPage/>} />
+        <Route path="/" element={<ProductUpload/>} />
         
      </Routes>
     )
