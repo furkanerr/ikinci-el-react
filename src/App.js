149e4e0fd803ba681f85a70ed006a2251af7b77b
furkaner <furkan08_@hotmail.com>
2022-04-30 01:59:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-
+import React from "react";
+import { ClotheCategoryProvider } from "./context/clotheCategoryContext";
 
 import Routers from "./routers/routers";
 
 function App() {
   return (
     <div className="App">
+      <ClotheCategoryProvider>
             <Routers />
+      </ClotheCategoryProvider>
     </div>
   );
 }
