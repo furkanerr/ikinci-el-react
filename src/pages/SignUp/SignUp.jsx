8140ba92f98e3b95984dd2fcb883228f448d8bfc
furkaner <furkan08_@hotmail.com>
2022-04-30 02:01:06 +0300

    register added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import RegisterStyle from "../../styles/RegisterStyle/RegisterStyle.module.css";
 import RegisterValidation from "../../constants/Validations/registerValidation";
+import api from '../../services/api';
 function SignUp() {
 
 
@@ -12,10 +13,49 @@ function SignUp() {
           password: "",
         },
         validationSchema: RegisterValidation,
-        onSubmit: (values) => {
-          console.log(values);
+        onSubmit: async (values) => {
+          try {
+          const response = await  api.Register({
+              username: values.password,
+              email: values.email,
+              password: values.password,
+            });
+            
+
+
+
+            console.log(response)
+          } 
+          catch (error) {
+            console.log(error);
+          }
+          
         },
       });
+    
+      const createCookie = (name, value, days) => {
+        var expires = "";
+        if (days) {
+          var date = new Date();
+          date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+          expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+      };
+      const readCookie = (name) => {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(";");
+        for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) === " ") c = c.substring(1, c.length);
+          if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+      };
+      const eraseCookie = (name) => {
+        createCookie(name, "", -1);
+      };
+
 
   return (
     <div className={RegisterStyle.Container}>
