3e09f72fa55a148b406ea5e3eb8d4031d3ad9b3a
furkaner <furkan08_@hotmail.com>
2022-04-30 01:58:42 +0300

    some endpoints added

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,56 @@ import axios from "axios";
 export default class Service {
    
 
-  
+  static async GetAllProducts() {
+    try {
+      const response = await axios.get("https://bootcamp.akbolat.net/products");
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+    static async GetProductById(id) {
+    try {
+      const response = await axios.get(`https://bootcamp.akbolat.net/products/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+   static async GetProductByCategory(category) {
+    try {
+      const response = await axios.get(`https://bootcamp.akbolat.net/products?category=${category}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async Login(input) {
+    try {
+      const response = await axios.post("https://bootcamp.akbolat.net/auth/local", input);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+    static async Register(input) {
+    try {
+      const response = await axios.post("https://bootcamp.akbolat.net/auth/local/register",input);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async GetCategories() {
+    try {
+      const response = await axios.get("https://bootcamp.akbolat.net/categories");
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
