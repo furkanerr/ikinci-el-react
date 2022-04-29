c4c98a922e99ed629b5aae3469b86d2a167c5d2b
furkaner <furkan08_@hotmail.com>
2022-04-30 01:57:46 +0300

    updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import HomePageStyle from "../../styles/HomePageStyle/HomePageStyle.module.css";
 import Card from "../../components/Card/Card";
 import ClotheTypes from "../../components/ClotheType/ClotheTypes";
 import Banner from '../../img/Banner1.png'
+import api from '../../services/api'
+import { useCategory } from "../../context/clotheCategoryContext";
 function HomePage() {
+  const {productsByCategory} = useCategory();
+  const [products,setProducts] = useState([])
+  useEffect(() => { 
+     const fetchData = async () => {
+      const response = await api.GetAllProducts();
+      setProducts(response)
+    }
+     fetchData();
+    console.log("çalıştı")
+
+  }, [])
+
   return (
     <div className={HomePageStyle.Container}>
       <Navbar />
@@ -19,20 +33,12 @@ function HomePage() {
         
       </div>
       <div className={HomePageStyle.CardContainer}>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        {
+          products?.map(product => (
+            <Card key={product.id} product={product}/>  
+            )) 
+        }
+     
       </div>
     </div>
   );
