bb799041bffa482d79eb7d64b9ae1525e718ab48
furkaner <furkan08_@hotmail.com>
2022-04-30 01:57:24 +0300

    product ifno added to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import CardStyle from '../../styles/CardStyle/CardStyle.module.css'
 import ProductImage  from '../../img/Image 5.png'
-function Card() {
+function Card({product}) {
+  let liraConvert = Intl.NumberFormat("tr-TR", {
+    style: "currency",
+    currency: "TRY",
+});
   return (
     <div className={CardStyle.Container}>
         <div className={CardStyle.Image}>
-          <img className={CardStyle.ProductImage} src={ProductImage}/>
+          <img className={CardStyle.ProductImage} src={`https://bootcamp.akbolat.net/${product.image?.formats?.small?.url}`}/>
         </div>
         <div className={CardStyle.Middle}>
-            <div className={CardStyle.Brand}>Lev'is</div>
+            <div className={CardStyle.Brand}>{product.brand}</div>
             <div className={CardStyle.ColorContainer}>
             <span className={CardStyle.ColorHeader}>Renk:</span>
-            <span className={CardStyle.Color}>Lacivert</span>
+            <span className={CardStyle.Color}>{product.color}</span>
             </div>
 
         </div>
-        <div className={CardStyle.Price}>1.999,00 TL</div>
+        <div className={CardStyle.Price}>{liraConvert.format(product.price)}</div>
 
     </div>
   )
