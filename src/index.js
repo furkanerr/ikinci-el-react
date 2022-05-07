589a1b4f416325ca83821022b413be0865ead79a
furkaner <furkan08_@hotmail.com>
2022-05-07 18:27:53 +0300

    react query added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
 
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      refetchOnMount:false,
+      refetchOnWindowFocus:false,
+    }
+
+}});
 
 ReactDOM.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
