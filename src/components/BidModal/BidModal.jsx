88dd1f898e7c078a35261fd3d95e59c4fa3a1077
furkaner <furkan08_@hotmail.com>
2022-05-07 18:22:00 +0300

    bugs fixed and validations added

--- src/components/BidModal/BidModal.jsx
+++ src/components/BidModal/BidModal.jsx
@@ -1,44 +1,118 @@
-import React from "react";
+/*Dependencies */
+import { useFormik } from "formik";
+import React, { useState } from "react";
+
+/**Style */
 import Modal from "../../styles/BidModalStyle/BidModalStyle.module.css";
-import ProductImg from '../../img/Image 5.png'
-function BidModal({toggleModal,isOpen}) {
-    let BidPercents = [20,30,40]
+
+/**Image */
+import undefinedProduct from "../../img/undefinedProduct.jpg";
+
+/**Context */
+import { useAuth } from "../../context/authContext";
+
+/**Services */
+import api from "../../services/api";
+
+/**Validations */
+import BidValidation from "../../constants/Validations/BidValidation";
+
+
+function BidModal({
+  toggleModal,
+  data,
+  setdeneme
+}) {
+  let BidPercents = [20, 30, 40];
+
+  const { user } = useAuth();
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleDisable = () => {
+    setIsChecked(!true);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      offerPrice: 0,
+    },
+    validationSchema: BidValidation,
+    onSubmit: async (values) => {
+      try {
+        const response = await api.MakeOffer({
+          offerPrice: values.offerPrice,
+          product: data.id,
+          users_permissions_user: user.data.id,
+        });
+        if (response.status === 200) {
+          setdeneme(X=>X+1);
+          console.log(response)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
   return (
     <div className={Modal.Container}>
-        
       <div className={Modal.Top}>
         <span className={Modal.BidText}>Teklif Ver</span>
         <span className={Modal.ExitIcon} onClick={toggleModal}>
-          <span >X</span>
+          <span>x</span>
         </span>
       </div>
       <div className={Modal.Product}>
-          <div className={Modal.ProductImgAndName}>
-               <div className={Modal.ImgAndName}> 
-                   <img src={ProductImg} alt=""/>
-                <span className={Modal.ProductName}>Pantolon</span></div>
-                <div className={Modal.ProductPrice}>
-            <span className={Modal.ProductPrice}>399,99</span>
-            </div>
+        <div className={Modal.ProductImgAndName}>
+          <div className={Modal.ImgAndName}>
+            <img
+              src={
+                data?.image?.formats?.medium == null
+                  ? undefinedProduct
+                  : `https://bootcamp.akbolat.net/${data.image?.formats?.medium?.url}`
+              }
+              alt=""
+            />
+            <span className={Modal.ProductName}>{data.name}</span>
           </div>
-            
+          <div className={Modal.ProductPrice}>
+            <span className={Modal.ProductPrice}>{data.price} TL </span>
+          </div>
+        </div>
       </div>
-      {
-        BidPercents.map((percent,index)=>{
-            return(
-                <div className={Modal.RadioButtons} key={index} >
-        {" "}
-        <input type="radio" value={percent} name={`checkbox`} />
-        <label >{`%${percent} Kadar Teklif Ver`}</label>
-      </div>
-            )
-            })
-      }
-      <div className={Modal.Bid}>
-        <input type="number" placeholder="Teklif Belirle" />
-      </div>
-      <div className={Modal.Button}>Onayla</div>
-      
+      <form onSubmit={formik.handleSubmit} style={{ display: "contents" }}>
+        {BidPercents.map((percent, index) => {
+          return (
+            <div className={Modal.RadioButtons} key={index}>
+              <input
+                type="radio"
+                name="offerPrice"
+                id={`Radio${percent}`}
+                checked={
+                  formik.values.offerPrice == (data.price * percent) / 100
+                }
+                onClick={handleDisable}
+                onChange={formik.handleChange}
+                value={(data.price * percent) / 100}
+              />
+              <label
+                for={`Radio${percent}`}
+              >{`%${percent} Kadar Teklif Ver`}</label>
+            </div>
+          );
+        })}
+        <div className={Modal.Bid} onClick={handleDisable}>
+          <input
+            type="number"
+            disabled={isChecked === true ? true : false}
+            name="offerPrice"
+            onChange={formik.handleChange}
+            value={formik.values.bid}
+            placeholder="Teklif Belirle"
+          />
+        </div>
+        <button className={Modal.Button}>Onayla</button>
+      </form>
     </div>
   );
 }
