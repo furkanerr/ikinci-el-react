f8b7e27f75b972284516a70b9cb487341278588e
furkaner <furkan08_@hotmail.com>
2022-05-07 18:25:22 +0300

    style changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,53 @@
+/**Dependencies */
 import React from "react";
+import { Link } from "react-router-dom";
+
+/**Style */
 import NavbarStyle from "../../styles/NavbarStyle/NavbarStyle.module.css";
+
+/**Icons */
 import PlusIcon from "../../constants/Icons/plusIcon";
 import PersonIcon from "../../constants/Icons/personIcon";
-import Logo from '../../img/Group 6607.png'
-import { Link } from "react-router-dom";
+import Logo from "../../img/Group 6607.png";
+
+
+/**Context */
+import { useAuth } from "../../context/authContext";
+
 function Navbar() {
+  const {  loggedIn } = useAuth();
   return (
     <div className={NavbarStyle.Navbar}>
-      <div className={NavbarStyle.Logo}>
-        <img className={NavbarStyle.LogoImg} src={Logo}/>
-      </div>
-      <div className={NavbarStyle.ButtonGroup}>
-        <div className={NavbarStyle.PlusIcon}>
-          <PlusIcon />
+      <div className={NavbarStyle.Container}>
+        <div className={NavbarStyle.Logo}>
+          <Link to="/">
+            <img className={NavbarStyle.LogoImg} src={Logo} />
+          </Link>
         </div>
-        {/* buraya Auth koşulu eklenecek. eğer giriş yapılmış ise Hesabım butonu gösterilecek */}
-        <div className={NavbarStyle.AddProductButton}>Ürün Ekle</div>
-        <div className={NavbarStyle.PersonIcon}>
-          <PersonIcon />
+        <div className={NavbarStyle.ButtonGroup}>
+          <div className={NavbarStyle.PlusIcon}>
+            <PlusIcon />
+          </div>
+          {/* buraya Auth koşulu eklenecek. eğer giriş yapılmış ise Hesabım butonu gösterilecek */}
+          <Link to={"/productupload"}>
+            <div className={NavbarStyle.AddProductButton}>Ürün Ekle</div>
+          </Link>
+          {loggedIn ? (
+            <>
+              {" "}
+              <div className={NavbarStyle.PersonIcon}>
+                <PersonIcon />
+              </div>{" "}
+              <Link to={"/account"}>
+                <div className={NavbarStyle.LoginButton}>Hesabım</div>
+              </Link>
+            </>
+          ) : (
+            <Link to={"/signin"}>
+              <div className={NavbarStyle.LoginButton}>Giriş Yap</div>
+            </Link>
+          )}
         </div>
-        <Link to={'/signin'}>
-        <div className={NavbarStyle.LoginButton}>Giriş Yap</div>
-        </Link>
       </div>
     </div>
   );
